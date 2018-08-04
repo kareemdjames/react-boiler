79aff71bdff8c87b1acba85c9653c7362325b4bc
Kareem James <kareemdjames@gmail.com>
2018-08-04 19:05:32 -0400

    completed initial boilerplate for react projects

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+  
+  render() {
+    return (
+      <div className="app">
+        <h1>Hello World</h1>
+        <h2>Kareem James</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
